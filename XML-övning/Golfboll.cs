4d0a82502985e2c9db05ac89e8683874ea05e5d3
GitCrey <assassincreydt@gmail.com>
2020-09-23 11:07:46 +0200

    serialize

---
+++ XML-övning/Golfboll.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace XML_övning
+{
+    public class Golfboll
+    {
+        public int x = 0;
+        public int y = 0;
+
+        public int speed = 10;
+    }
+}
---
+++ XML-övning/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XML_övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Golfboll mini = new Golfboll();
+
+            mini.speed = 5;
+
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Golfboll));
+
+            FileStream myFile = File.Open("golfboll.xml", FileMode.OpenOrCreate);
+
+            serializer.Serialize(myFile, mini);
+
+            //Golfboll mini = (Golfboll) serializer.Dezerialize(myFile):
+
+            myFile.Close();
+
+            Console.WriteLine("What speed is the golf ball gonna be? Please don't question this question...questception :o");
+            
+            string input = Console.ReadLine();
+
+            int num = 0;
+
+            bool success = int.TryParse(input, out num);
+
+            Console.WriteLine(num + 1);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
